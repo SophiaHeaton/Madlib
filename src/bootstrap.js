da52a33650b703b655ccee6d9b2fa246e1d32085
Sophia Heaton <saheaton1@gmail.com>
2018-07-28 18:55:42 -0600

    Commit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Home from './components/app';
+import Home from './components/home';
 
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
 function main() {
