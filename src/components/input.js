c3b22adee8dd11aff46a06bad15356747945cc16
Sophia Heaton <saheaton1@gmail.com>
2018-07-28 19:13:16 -0600

    Commit

---
+++ src/components/input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Input extends Component {
+    render() {
+        return (
+            <div className="input">
+                <input />
+                <label>Title</label>
+            </div>
+        )
+    }
+}
+
+export default Input;
